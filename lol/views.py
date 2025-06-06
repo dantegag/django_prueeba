521ce1d4e504ada6c3260bd3e4d6a83cc90ed95a
Dante Gaggero <dante.gaggero@gmail.com>
2025-06-06 01:56:45 -0300

    cambio de templates

--- lol/views.py
+++ lol/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {"mensaje": "bienvenido a mi app"}
+    context = {"mensaje": "PAGINA SOBRE LETI LA MAS HERMOSA"}
     return render(request, "lol/index.html", context)
