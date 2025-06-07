541a36be6f20b3f4eacbbe68fbc75f248220942b
Dante Gaggero <dante.gaggero@gmail.com>
2025-06-07 18:19:27 -0300

    segunda linea

--- lol/views.py
+++ lol/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {"mensaje": "Prueba django"}
+    context = {"mensaje": "Prueba django",
+               "mensaje2": "segundo linea"}
     return render(request, "lol/index.html", context)
